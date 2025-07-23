26ac852dab4bd8b2661590470648dd2b9b436363
Preeti-deora <136837983+Preeti-deora@users.noreply.github.com>
2025-07-23 14:15:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from utils.helpers import predict
+
+st.set_page_config(page_title="🌿 Plant Disease Classifier", layout="centered")
+
+st.title("🌱 Plant Disease Classification using ML")
+st.markdown("Upload a plant leaf image and get its predicted disease category.")
+
+uploaded = st.file_uploader("Choose a leaf image", type=["jpg", "png", "jpeg"])
+
+if uploaded:
+    st.image(uploaded, caption="Uploaded Image", use_column_width=True)
+    with st.spinner('Predicting...'):
+        label = predict(uploaded)
+    st.success(f"**Prediction:** {label}")
 
