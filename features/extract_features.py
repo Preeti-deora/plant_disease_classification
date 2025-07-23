569bd585698be53086b2186545c3d4d1b674176d
Preeti-deora <136837983+Preeti-deora@users.noreply.github.com>
2025-07-24 01:36:28 +0530

    Update extract_features.py

--- features/extract_features.py
+++ features/extract_features.py
@@ -2,22 +2,15 @@ import cv2
 import numpy as np
 from skimage.feature import hog
 
-def extract_features(image):
-    image = image.resize((128, 128))  # Must match training
-    image_np = np.array(image)
+def extract_features(img):
+    img = img.resize((128, 128)).convert("RGB")
+    gray_img = img.convert("L")
+    feature, _ = hog(np.array(gray_img),
+                     orientations=9,
+                     pixels_per_cell=(16, 16),
+                     cells_per_block=(2, 2),
+                     block_norm='L2-Hys',
+                     visualize=True)
+    return feature
 
-    if image_np.ndim == 3 and image_np.shape[2] == 3:
-        gray_image = rgb2gray(image_np)
-    elif image_np.ndim == 2:
-        gray_image = image_np
-    else:
-        raise ValueError("Unsupported image format")
-
-    hog_features = hog(
-        gray_image,
-        pixels_per_cell=(16, 16),
-        cells_per_block=(2, 2),
-        feature_vector=True
-    )
-    return hog_features
 
